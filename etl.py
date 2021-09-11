b01bc1e710087b34f79fc2eb8670a4e45b2b2b2c
root <juferafo@hotmail.com>
2021-09-11 14:29:44 +0000

    Tables creation finished and tested OK

--- etl.py
+++ etl.py
@@ -14,16 +14,24 @@ os.environ['AWS_SECRET_ACCESS_KEY']=config['AWS_SECRET_ACCESS_KEY']
 
 
 def create_spark_session():
+    """
+    This method returns the SparkSession object. It adds the Apache Hadoop AWS library as configuration parameter.
+    
+    Returns:
+        spark (pyspark.sql.session.SparkSession)
+    """
+    
     spark = SparkSession \
         .builder \
         .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:2.7.0") \
         .getOrCreate()
+    
     return spark
 
 
 def process_song_data(spark, input_data, output_data):
     # get filepath to song data file
-    song_data = 
+    song_data =
     
     # read song data file
     df = 
